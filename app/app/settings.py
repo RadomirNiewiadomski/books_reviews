49c74470bd7281544da0773e5d35c342fb310db0
Radomir Niewiadomski <radomirn96@gmail.com>
2024-06-17 21:29:21 +0200

    add own User app with own User model etc and add token authentication

--- app/app/settings.py
+++ app/app/settings.py
@@ -39,6 +39,7 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
     'core',
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -130,3 +131,5 @@ STATIC_ROOT = '/vol/web/static'
 # https://docs.djangoproject.com/en/4.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+AUTH_USER_MODEL = 'user.User'
